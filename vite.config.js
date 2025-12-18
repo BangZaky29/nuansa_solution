aafd6a2ca48629921ea5c5208b10120936800357
BangZaky29 <bangzaky0029@gmail.com>
2025-12-18 12:01:03 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nuansa_solution/',
+  base: '/',   // gunakan root domain
 })
